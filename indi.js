fcc55588ae01cea08d38408eed479cef741c2126
VISHNU <vvichugit@gmail.com>
2019-07-06 23:54:52 -0500

    commit of Grid Systems

---
+++ indi.js
@@ -0,0 +1,20 @@
+display = () => {
+    var bottNav = document.querySelector('.bottomNav');
+    
+//    if(bottNav.value == "bottomNav show"){
+//        bottNav.classList.remove('show');
+//        bottNav.classList.add('hide');
+//        
+//    }else {
+//        bottNav.classList.add('show');
+//             
+//    }
+    
+
+    bottNav.classList.toggle("show");
+     
+
+
+   console.log(bottNav.classList);
+    
+}
